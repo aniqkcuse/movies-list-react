8e042b0d4806b8c33b40d0eb311bc51ca2e397f7
Ken Casique <kencasique@gmail.com>
2024-01-07 19:14:26 -0400

    Created cardmovie component

---
+++ src/components/CardMovie.tsx
@@ -0,0 +1,11 @@
+function CardMovie() {
+  return (
+    <div className="card-movie-base">
+      <h1>Title of the movie</h1>
+      <p>Description of the movie</p>
+      <small>Author of the movie</small>
+    </div>
+  );
+}
+
+export default CardMovie;
